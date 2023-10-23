57c8204217c368fa29e1e4047db1705f21c7e68c
techieruoj <jourmedi@gmail.com>
2023-10-23 15:41:20 -0400

    creating title initial setup

---
+++ adventure.js
@@ -0,0 +1,24 @@
+alert ("hi")
+// method -a function defined within an object
+//alert and prompt are methods of the Window object,a built in object in the JS language
+
+//alert
+//alert("I am using HTML and JS together!");
+
+//The alert()method is used when you want information to come through to the user.
+
+//The alert()method is used when you want information to come to the user.
+//alert("I am using HTML and JS together!!!");
+
+//prompt
+
+//The prompt ()method displays a dialog box that prompts the user for input.
+
+//prompt()method returns the input value if the user clicks "OK" , otherwise it returns null.
+//const userName = prompt("Enter your name");
+console.log(userName);
+
+//parseInt
+
+const num1 = parseInt(prompt("Enter a number"));
+console.log(num1 /2);
